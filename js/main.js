70813aabdfa316a592bbd4ca2b1085d566634a25
Akbota <akbotamold@gmail.com>
2023-12-28 01:03:26 +0600

    'add'

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth',
+                speed: 1000
+            });
+        }
+    });
+});
+
 const toggleMenu = () => {
   document.getElementById("mobile").classList.toggle("active");
   document.body.classList.toggle("lock");
@@ -148,3 +165,5 @@ document.querySelectorAll('.btn-b').forEach((button) => {
         toggleModal();
     });
 });
+
+
